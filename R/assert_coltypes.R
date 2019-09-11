e17e52694b2fac5b72e61a517b995e9f55354f0c
Max Czapanskiy <maxczap@stanford.edu>
2019-09-11 15:33:12 -0700

    Add assert_coltypes
    Also adds me to DESCRIPTION as a contributor

---
+++ R/assert_coltypes.R
@@ -0,0 +1,38 @@
+#' Assert that a data.frame's columns are certain types
+#'
+#' Given a data.frame or data.table object, assert that all columns in the names of the coltypes 
+#' argument match the types of the elements of the coltypes argument. 
+
+#' @param data A data.frame or data.table
+#' @param coltypes List with names corresponding to columns in \emph{data}. The types of the columns in \emph{data} 
+#'   will be tested against types of the elements in coltypes.
+#' @param quiet Do you want to suppress the printed message when a test is passed? Default = F.
+
+#' @return Throws error if test is violated.
+#' @export
+
+#' @examples
+#' # Should pass
+#' assert_coltypes(CO2, list(Plant = integer(0), conc = double(0)))
+#' # Should fail
+#' assert_coltypes(CO2, list(Plant = character(0), conc = character(0))
+
+assert_coltypes <- function(data, coltypes, quiet=FALSE) {
+  # Do all specified colnames exist in data frame?
+  non_df_cols <- colnames(coltypes)[!(names(coltypes) %in% colnames(data))]
+  if(length(non_df_cols) > 0) {
+    stop(paste0("These columns exist in colnames but not in your dataframe: ",
+                paste(non_df_cols, collapse=" ")))
+  }
+  
+  # Do all column types match?
+  typematch <- sapply(data[, names(coltypes)], typeof) == sapply(coltypes, typeof)
+  if(any(!typematch)) {
+    stop(paste0("These columns in your data frame have different types than coltypes: ",
+                paste(names(coltypes)[!typematch], collapse = " ")))
+  }
+  
+  if(!quiet) {
+    print("All column types match")
+  }
+}
